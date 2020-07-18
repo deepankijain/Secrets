acaa1d374609864cac46c8c2ef7e38ca6a18f7a7
Deepanki Jain <deepankijain@gmail.com>
2020-07-18 22:43:43 +0530

    Allow users to submit a secret

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -83,12 +84,24 @@ app.get("/register",function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret":{$ne:null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
-})
+});
 
 app.get('/logout', function(req, res){
   req.logout();
@@ -125,7 +138,21 @@ app.post("/login", function(req, res){
   });
 });
 
-
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 
 
 app.listen(3000, function(){
